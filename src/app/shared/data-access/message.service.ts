f04331ee6dc4eb4cc1e95db406f850a99bba8f63
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2023-03-21 10:53:35 +0200

    messsage service

---
+++ src/app/shared/data-access/message.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {
+  addDoc,
+  collectionData,
+  Firestore,
+  limit,
+} from '@angular/fire/firestore';
+import { collection, orderBy, query } from '@firebase/firestore';
+import { map, Observable } from 'rxjs';
+import { IMessage } from '../interfaces/message.interface';
+
+@Injectable({ providedIn: 'root' })
+export class MessageService {
+  constructor(private firestore: Firestore) {}
+
+  public getMessages() {
+    const messages = query(
+      collection(this.firestore, 'messages'),
+      orderBy('created', 'desc'),
+      limit(50)
+    );
+    return collectionData(messages, { idField: 'id' }).pipe(
+      map((messages) => [...messages].reverse())
+    ) as Observable<IMessage[]>;
+  }
+
+  public addMessaged(message: string) {
+    const newMessage: IMessage = {
+      author: 'test@gmail.com',
+      content: message,
+      created: Date.now().toString(),
+    };
+    const msgCollection = collection(this.firestore, 'message');
+    addDoc(msgCollection, newMessage);
+  }
+}
