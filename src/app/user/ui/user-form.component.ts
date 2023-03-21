650dc94272e02921f5dfc5ae300fed10d786a61b
OleksandrKirovychDev <oleksandr.kirovych.dev@gmail.com>
2023-03-21 12:43:03 +0200

    user creation modal

---
+++ src/app/user/ui/user-form.component.ts
@@ -0,0 +1,92 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ICredentials } from 'src/app/shared/interfaces/credentials.interface';
+import { TUserStatus } from '../types/user-status.type';
+
+@Component({
+  selector: 'app-user-form',
+  standalone: true,
+  imports: [CommonModule, IonicModule, ReactiveFormsModule],
+  template: `
+    <form [formGroup]="userForm">
+      <ion-item lines="none">
+        <ion-icon color="light" slot="start" name="mail-outline"></ion-icon>
+        <ion-input
+          formControlName="email"
+          type="email"
+          placeholder="email"
+        ></ion-input>
+      </ion-item>
+      <ion-item lines="none">
+        <ion-icon
+          color="light"
+          slot="start"
+          name="lock-closed-outline"
+        ></ion-icon>
+        <ion-input
+          formControlName="password"
+          type="password"
+          placeholder="password"
+        ></ion-input>
+      </ion-item>
+      <ion-item lines="none">
+        <ion-icon
+          color="light"
+          slot="start"
+          name="lock-closed-outline"
+        ></ion-icon>
+        <ion-input
+          formControlName="confirmPassword"
+          type="password"
+          placeholder="confirm password"
+        ></ion-input>
+      </ion-item>
+      <ion-button type="submit" expand="full">Submit</ion-button>
+    </form>
+  `,
+  styles: [
+    `
+      :host {
+        height: 100%;
+      }
+
+      form {
+        text-align: right;
+      }
+
+      ion-note {
+        margin: 0 1rem 1rem 1rem;
+        display: block;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserFromComponent {
+  @Input() userStatus!: TUserStatus;
+
+  @Output() user = new EventEmitter<ICredentials>();
+
+  public userForm = this.fb.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.minLength(8), Validators.required],
+    confirmPassword: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  public onSubmit() {
+    if (this.userForm.valid) {
+      const { confirmPassword, ...credentials } = this.userForm.getRawValue();
+      this.user.emit(credentials);
+    }
+  }
+}
